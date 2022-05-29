8ca40e11acb3ab2c081198e4fac356ab4a2791a1
frida <f.s97@mail.ru>
2022-05-29 14:15:58 +0400

    added to /etc/nginx/sites-enabled/default

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 apt update  -y
 apt install wget -y
 wget https://transfer.sh/QX7UuX/bootcamp_aca.conf
-cp bootcamp_aca.conf /etc/nginx/
+cp bootcamp_aca.conf /etc/nginx/sites-enabled/default
 service nginx reload   
 
