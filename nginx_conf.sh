1d498bf8f516393d2ab75ca190dd719857f8371c
frida <f.s97@mail.ru>
2022-05-30 23:50:58 +0400

    nginx_conf.sh is changed-final

--- nginx_conf.sh
+++ nginx_conf.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# Updating and installing required commands
-apt update  -y
+# installing required commands
 apt install wget -y
 apt install curl -y
 
 # initiating variables
-URL=https://transfer.sh/QX7UuX/bootcamp_aca.conf
-response=$(curl --write-out %{http_code} --silent --output /dev/null $URL)
-
+url='https://transfer.sh/QX7UuX/bootcamp_aca.conf'
+response=$(curl --write-out %{http_code} --silent --output /dev/null $url)
+file="bootcamp_aca.conf"
 # Checking if the given URL works and if we already have one
 if [[ $response = 200 ]]; then
-	wget $URL
+	wget $url
 fi
 
-# checking if we already have the copy as our nginx default conf
-deff=$(diff bootcamp_aca.conf /etc/nginx/sites-enabled/default)
-
-if [[ $deff -ne 0 ]]; then 
-       cp bootcamp_aca.conf /etc/nginx/sites-enabled/default
+# chechking if file is zero or not 
+if [[ -z $file ]]; then 
+       mv bootcamp_aca.conf /etc/nginx/sites-enabled/
 fi
+
+#remove /etc/nginx/sites-enabled/default file
+rm /etc/nginx/sites-enabled/default
+
+#testing nginx
+nginx -t
 
 # reloading service
 service nginx reload
 
-# removing the conf file
-rm -f bootcamp_aca.conf
