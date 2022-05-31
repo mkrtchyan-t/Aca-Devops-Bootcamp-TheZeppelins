3219d1a5b5105205a764a04d197e60cbd94e58b1
tomma <tomma8175@gmail.com>
2022-05-31 13:32:58 +0400

    minor bug fixes

--- print_stat.sh
+++ print_stat.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
-# we are using while loop to recompile the code for every 10 seconds
+# we are using while loop to recompile the code
 while true
 do
 
-# initiating memory usage
-mem="$(free -m | tr -s " " | cut -d " " -f 3,2 | tr " " "/" | head -2 | tail -1)MB"
+# initiating memory usage(MB)
+memtotal=$(cat /proc/meminfo | grep -i memtotal | tr -s " " | cut -d " " -f 2 | awk '{printf $1/1024}')
+memfree=$(cat /proc/meminfo | grep -i memfree | tr -s " " | cut -d " " -f 2 | awk '{printf $1/1024}')
 
 # initiating disk usage
-disk=$(df -h | head -4 | tail -1 | tr -s " " | cut -d " " -f 5)
+disk=$(df -h / | tail -1 | tr -s " " | cut -d " " -f 5)
 
 # initiating cpu usage
-cpu=$(top -bn1 | grep load | tr -s " " | cut -d " " -f 12)
+cpu=$(top -bn1 | grep -i "cpu(s)" | tr -s " " | cut -d " " -f 8 | awk '{printf 100-$1}')
 
 # redirecting statistics to our index.html file
-echo "Memory Usage: $mem \n Disk Usage: $disk \n CPU Usage: $cpu %" > /var/www/bootcamp_aca/index.html
+echo "Memory Usage: ${memfree}/${memtotal}MB <br>
+      Disk Usage: $disk <br>
+      CPU Usage: $cpu % <br>"	> /var/www/bootcamp_aca/index.html
 
 # setting "code restart" time to 10 seconds
 sleep 10
