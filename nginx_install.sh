0d6e4d40fca7c95a770a3949c49bf48d164e990f
Hakob9 <1hakob@gmail.com>
2022-05-31 16:19:23 +0400

    final changes are done

--- nginx_install.sh
+++ nginx_install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-# updating, installing and starting nginx and required libraries
+# updating apt
 apt update -y
+#checking apt version
+apt -v
+#installing nginx
 sudo apt install nginx -y
-ufw allow 'Nginx Full'
+#checking nginx version
+nginx -v
+#using uncomplicated firewall utility to configure brandmauer 
+ufw allow 'Nginx HTTP'
+#checking the changes
+ufw status
+#checking how webserver services work
 systemctl start nginx
 
